9eadf99daeb2038440df953efe049db73a2f6150
bnrpmbd <banarpambudi094@gmail.com>
2025-09-01 13:13:04 +0700

    feat: blade layout, component, clubs view

--- resources/views/clubs/index.blade.php
+++ resources/views/clubs/index.blade.php
@@ -2,8 +2,8 @@
 @section('content')
  <h2 style="margin-bottom:1rem">Daftar Klub</h2>
  <section class="grid">
- @foreach($clubs as $club)
- <x-club-card :club="$club" />
- @endforeach
+  @foreach($clubs as $club)
+   <x-club-card :club="$club" />
+  @endforeach
  </section>
 @endsection
